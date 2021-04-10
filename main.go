63c73f01fa1e2087c2637361cc8118addd5760fd
icedpenguin0504 <icedpenguin0504@gmail.com>
2021-04-10 18:33:47 +0900

    add: Enable you to input numbers

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"strconv"
+
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
@@ -9,8 +13,38 @@ import (
 func main() {
 	app := app.New()
 	window := app.NewWindow("Calc")
+
+	formula := ""
+	label := widget.NewLabel(formula)
+
+	pushNum := func(n int) {
+		// 数式の先頭が0とならないように
+		if formula == "" && n == 0 {
+			return
+		}
+		formula += strconv.Itoa(n)
+		label.SetText(formula)
+	}
+
 	window.SetContent(
-		widget.NewLabel("Hello"),
+		widget.NewVBox(
+			label,
+			fyne.NewContainerWithLayout(
+				layout.NewGridLayout(3),
+				widget.NewButton("1", func() { pushNum(1) }),
+				widget.NewButton("2", func() { pushNum(2) }),
+				widget.NewButton("3", func() { pushNum(3) }),
+				widget.NewButton("4", func() { pushNum(4) }),
+				widget.NewButton("5", func() { pushNum(5) }),
+				widget.NewButton("6", func() { pushNum(6) }),
+				widget.NewButton("7", func() { pushNum(7) }),
+				widget.NewButton("8", func() { pushNum(8) }),
+				widget.NewButton("9", func() { pushNum(9) }),
+				widget.NewButton("0", func() { pushNum(0) }),
+				layout.NewSpacer(),
+				layout.NewSpacer(),
+			),
+		),
 	)
 
 	app.Settings().SetTheme(theme.DarkTheme())
